bbeb7075dcc94e596d2609191cf8b24e4f995d1a
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-01 10:31:50 +0300

    Сделано задание 3

--- Task03.py
+++ Task03.py
@@ -5,3 +5,29 @@
 📌 Если переданы два параметра, считаем step=1.
 📌 Если передан один параметр, также считаем start=1.
 """
+
+
+class Factorial:
+
+    def __init__(self, stop: int, start: int = 1, step: int = 1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self._current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._current > self.stop:
+            raise StopIteration
+        res = 1
+        for num in range(1, self._current + 1):
+            res *= num
+        self._current += self.step
+        return res
+
+
+factorial = Factorial(5, 2, 2)
+for f in factorial:
+    print(f)
