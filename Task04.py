bcb2adcd0245b62b1fd8d024bb8cf4a26a0c349f
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-01 10:39:45 +0300

    Сделано задание 5

--- Task04.py
+++ Task04.py
@@ -43,29 +43,12 @@ class Rectangle:
         if value >= 0:
             self.__height = value
 
-    def __add__(self, other):
-        return Rectangle((self.perimeter() + other.perimeter()) / 4)
-
-    def __sub__(self, other):
-        if other.perimeter() > self.perimeter():
-            return Rectangle(0)
-        return Rectangle((self.perimeter() - other.perimeter()) / 4)
-
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __eq__(self, other):
-        return self.area() == other.area()
-
-    def __le__(self, other):
-        return self.area() <= other.area()
-
     def __str__(self):
         return f"Прямоугольник со сторонами {self.__height} и {self.__width}"
 
-    def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
-
 
 if __name__ == "__main__":
-    r_1 = Rectangle(5)
+    r = Rectangle(5, 4)
+    print(r)
+    r.width = 7
+    print(r)
--- Task05.py
+++ Task05.py
@@ -2,3 +2,51 @@
 Задание №5
 📌 Доработаем прямоугольник и добавим экономию памяти для хранения свойств экземпляра без словаря __dict__.
 """
+
+class Rectangle:
+    """Класс, реализующий прямоугольник"""
+
+    __slots__ = ["__width", "__height"]
+
+    def __init__(self, width, height = None):
+        self.__width = width
+        if height is None:
+            self.__height = width
+        else:
+            self.__height = height
+
+    def perimeter(self):
+        """Метод вычисления периметра прямоугольника"""
+        return 2 * (self.__height + self.__width)
+
+    def area(self):
+        """Метод вычисления площади прямоугольника"""
+        return self.__width * self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value >= 0:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value >= 0:
+            self.__height = value
+
+    def __str__(self):
+        return f"Прямоугольник со сторонами {self.__height} и {self.__width}"
+
+
+if __name__ == "__main__":
+    r = Rectangle(5, 4)
+    print(r)
+    r.width = 7
+    print(r)
